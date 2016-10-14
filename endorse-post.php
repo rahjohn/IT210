16102dca31b62a3f768f0ba17c20b5035ee29aa7
Rachel Johnson <rachelelise527@gmail.com>
2016-10-14 16:12:34 -0600

    this one is for you bryce

--- endorse-post.php
+++ endorse-post.php
@@ -1,10 +1,11 @@
 <?php
     $jsonFile = "JSON.JSON";
     $arrayToAdd = array("name" => $_POST["name"], "date" => $_POST["date"], "endorse" => $_POST["endorse"]);
-    $arrayToAdd = json_encode($arrayToAdd);
+    $arrayToAdd = ", \n  " . json_encode($arrayToAdd) . "]";
     echo $arrayToAdd;
     $handle=fopen($jsonFile, 'r+');
     fseek($handle,-2,SEEK_END);
     fwrite($handle, $arrayToAdd);
     fclose($handle);
+    header ("Location: endorsements.php");
 ?>
