59e42ad5aa654e11cac83e453783d9d71d12aad2
Rachel Johnson <rachelelise527@gmail.com>
2016-10-14 16:02:40 -0600

    adding to JSON file but not correctly

---
+++ endorse-post.php
@@ -0,0 +1,10 @@
+<?php
+    $jsonFile = "JSON.JSON";
+    $arrayToAdd = array("name" => $_POST["name"], "date" => $_POST["date"], "endorse" => $_POST["endorse"]);
+    $arrayToAdd = json_encode($arrayToAdd);
+    echo $arrayToAdd;
+    $handle=fopen($jsonFile, 'r+');
+    fseek($handle,-2,SEEK_END);
+    fwrite($handle, $arrayToAdd);
+    fclose($handle);
+?>
